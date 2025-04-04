7a61920980e92b0f6b466186c3494ab50864d6b0
Frech <frechwarren120415@gmail.com>
2025-04-05 05:54:19 +0800

    created reusable logic file

---
+++ src/lib/userAuthentication.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+export async function authUser() {
+  const secretKey = process.env.JWT_SECRET;
+
+  // Check if the secret key is defined
+  if (!secretKey) {
+    console.error("JWT_SECRET is not defined");
+    return null;
+  }
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("dentalserviceapp")?.value;
+
+  // Return null if no token is found
+  if (!token) {
+    return null;
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    return decoded;
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    return null;
+  }
+}
