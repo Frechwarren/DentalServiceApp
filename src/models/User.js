ddf6a6704883d4c27c00f329039bec3e1b165dc5
Frech <frechwarren120415@gmail.com>
2025-04-05 05:54:45 +0800

    create user model

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
