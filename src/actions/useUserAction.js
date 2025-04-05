13cb16fd4fa3e5d0eae887cb106d2fb2f6b848cb
Frech <frechwarren120415@gmail.com>
2025-04-06 05:12:39 +0800

    created a getUserData controller

--- src/actions/useUserAction.js
+++ src/actions/useUserAction.js
@@ -25,3 +25,38 @@ export async function userLogin(formData) {
 
   return data;
 }
+
+export async function getUser(userId) {
+  try {
+    console.log("userId", userId);
+    
+    const response = await fetch(`/api/users/${userId}`, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      switch (response.status) {
+        case 400:
+          throw new Error(
+            errorData.message || "Validation error. Please check your input."
+          );
+        case 401:
+          throw new Error("Unauthorized. Please log in to book a service.");
+        case 500:
+          throw new Error("Internal server error. Please try again later.");
+        default:
+          throw new Error("An unexpected error occurred. Please try again.");
+      }
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error booking service:", error);
+    throw new Error(
+      error.message || "Failed to book the service. Please try again."
+    );
+  }
+}
